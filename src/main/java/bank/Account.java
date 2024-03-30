dd2cd323093b8586c09051ea446be8bb476edf2c
DDTraining <103615329+DDTraining@users.noreply.github.com>
2024-03-30 03:11:42 +0000

    modified:   src/main/java/bank/Account.java

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -8,7 +8,7 @@ public class Account {
   public Account( int id, String type, double balance){
     setId(id);
     setType (type);
-    setBalance(balance)
+    setBalance(balance);
   }
 
   public int getId() {
